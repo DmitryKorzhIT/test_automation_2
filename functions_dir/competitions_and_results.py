12ec18ac5eb9fb8c0ee64b29f957a5112e58e781
DmitryKorzhIT <DmitryKorzhIT@gmail.com>
2022-08-12 19:06:33 +0300

    #14 The test number five is partly done. Now it collects data from each competition row through the year.

--- functions_dir/competitions_and_results.py
+++ functions_dir/competitions_and_results.py
@@ -77,32 +77,33 @@ class CompetitionsAndResults(Base):
             accordion_element.click()
             month_element = self.find_element(By.CLASS_NAME, 'month--open')
 
-            month_name = month_element.find_element(By.CLASS_NAME, 'month__name')
-            month_name_text = month_name.text
+            competitions_list = month_element.find_elements(By.CLASS_NAME, 'info__row.row.no-gutters.event-item')
 
-            competition_row = month_element.find_element(By.CLASS_NAME, 'info__row.row.no-gutters.event-item')
+            for competition_row in competitions_list:
+                month_name = month_element.find_element(By.CLASS_NAME, 'month__name')
+                month_name_text = month_name.text
 
-            competition_dates = competition_row.find_element(By.CLASS_NAME, 'col.event-item__date')
-            competition_dates_text = competition_dates.text
+                competition_dates = competition_row.find_element(By.CLASS_NAME, 'col.event-item__date')
+                competition_dates_text = competition_dates.text
 
-            competition_medal = competition_row.find_element(By.CLASS_NAME, 'col.event-item__medal')
-            competition_medal_text = competition_medal.text
+                competition_medal = competition_row.find_element(By.CLASS_NAME, 'col.event-item__medal')
+                competition_medal_text = competition_medal.text
 
-            competition_type = competition_row.find_element(By.CLASS_NAME, 'col.event-item__type')
-            competition_type_text = competition_type.text
+                competition_type = competition_row.find_element(By.CLASS_NAME, 'col.event-item__type')
+                competition_type_text = competition_type.text
 
-            competition_type_red = competition_type.find_element(By.CLASS_NAME, 'red')
-            competition_type_red_text = competition_type_red.text
+                competition_type_red = competition_type.find_element(By.CLASS_NAME, 'red')
+                competition_type_red_text = competition_type_red.text
 
-            competition_athletes = competition_row.find_element(By.CLASS_NAME, 'col.event-item__athletes')
-            competition_athletes_text = competition_athletes.text
+                competition_athletes = competition_row.find_element(By.CLASS_NAME, 'col.event-item__athletes')
+                competition_athletes_text = competition_athletes.text
 
-            print(f'Month_name: {month_name_text}; '
-                  f'Dates: {competition_dates_text}; '
-                  f'Medal: {competition_medal_text}; '
-                  f'Type_red: {competition_type_red_text}; '
-                  f'Type: {competition_type_text}; '
-                  f'Athletes: {competition_athletes_text}.')
+                print(f'Month_name: {month_name_text}; '
+                      f'Dates: {competition_dates_text}; '
+                      f'Medal: {competition_medal_text}; '
+                      f'Type_red: {competition_type_red_text}; '
+                      f'Type: {competition_type_text}; '
+                      f'Athletes: {competition_athletes_text}.')
 
             accordion_element.click()
 
